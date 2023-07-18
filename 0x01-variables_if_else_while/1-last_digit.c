a774ec7df79cba6a306528959ef5a87a7edc5ba9
dreamboy202 <chigbuikechukwu@gmail.com>
2023-07-17 20:46:38 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	i  = n % 10;
+	if (i > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, i);
+	if (i == 0)
+		printf("Last digit of %d is %d and is 0\n", n, i);
+	if (i < 6 && i != 0)
+		printf("Last digit of %d is %d and is Less thsn 6 and not 0\n", n, i);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main()
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
