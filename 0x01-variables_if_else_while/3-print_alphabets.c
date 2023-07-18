487777aa7b4d4bc4a25b691ab8d6d6eddcaee963
dreamboy202 <chigbuikechukwu@gmail.com>
2023-07-17 21:16:46 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char w;
+	char x;
+
+	w = 'a';
+	x = 'A';
+	while
+		(w <= 'z') {
+			putchar(w);
+			w++;
+		}
+	while
+		(x <= 'Z') { 
+			putchar (x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
