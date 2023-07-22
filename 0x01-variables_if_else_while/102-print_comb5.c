c1b95f1ed531ab99e2b5121e77838ce76e09bbd8
dreamboy202 <chigbuikechukwu@gmail.com>
2023-07-22 04:11:14 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 100; num1++)
+	{
+		for (num2 = 0; num2 < 100; num2++)
+		{
+			if (num1 <= num2)
+			{
+				int tens1 = num1 / 10;
+				int ones1 = num1 % 10;
+				int tens2 = num2 / 10;
+				int ones2 = num2 % 10;
+
+				putchar(tens1 + '0');
+				putchar(ones1 + '0');
+				putchar(' ');
+				putchar(tens2 + '0');
+				putchar(ones2 + '0');
+
+				if (num1 < 98 || num2 < 99) /* To print ',' and ' ' for all combinations except 99 99 */
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
