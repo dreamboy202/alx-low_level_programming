fa22d01de85b99ef2aa14614e3631d4df339959c
dreamboy202 <chigbuikechukwu@gmail.com>
2023-07-22 22:17:39 +0100

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,15 @@ void jack_bauer(void)
 {
 	int i, j;
 
+
 	i = 0;
 
+
 	while (i < 24)
+	{
 		j = 0;
 		while (j < 60)
-		while (j < 60)
+		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
@@ -22,6 +25,6 @@ void jack_bauer(void)
 			_putchar('\n');
 			j++;
 		}
-		++;
+		i++;
 	}
-}
+}	
