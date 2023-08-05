2d45cc819f5f65e24863f0e1222e84e43d4f002d
dreamboy202 <chigbuikechukwu@gmail.com>
2023-08-05 23:42:23 +0100

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - function like puts();
+ *@s: input
+ *Return: Always 0 (Success)
+ */
+{
+	if (*s)
+	{
+		_putchar(*s)
+		_puts_recursion(s +1);
+	}
+	else
+		_putchar('\n');
+}
