1cf4180185d0d8d57aa37a9c1550a1d94123d362
dreamboy202 <chigbuikechukwu@gmail.com>
2023-08-07 06:08:38 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -26,16 +26,16 @@ int main(int argc, char *argv[])
 
 	result = 0;
 
-	if (num < 0)		
+	if (num < 0)
 	{
 		printf("0\n");
 
 		return (0);
 	}
-		
+
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		while (num >= coins[j])	
+		while (num >= coins[j])
 		{
 			result++;
 			num -= coins[j];
